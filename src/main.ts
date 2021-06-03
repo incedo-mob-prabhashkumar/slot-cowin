cac16ec496a8e28c087e18c7b6a5a90c6068f0dc
Prabhas Singh08 <prsing08@in.ibm.com>
2021-06-04 00:22:52 +0530

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-
+import * as bodyParser from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
 
   app.enableCors();
   
