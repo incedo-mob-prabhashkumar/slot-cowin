c6abe0e4592f8dcfe204ab761365e22d7964f808
Prabhas Singh08 <prsing08@in.ibm.com>
2021-06-03 18:23:34 +0530

    first commit

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port);
+  });
 }
 bootstrap();
