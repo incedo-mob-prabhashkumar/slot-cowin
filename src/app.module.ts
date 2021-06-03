3321570d041f230d9e033c2c78da496111b40165
Prabhas Singh08 <prsing08@in.ibm.com>
2021-06-03 17:54:36 +0530

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module,HttpService, HttpModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScheduleModule } from '@nestjs/schedule';
-import {LogsController} from 'src/modules/logs/logs.controller'
-import {LogsService} from 'src/modules/logs/logs.service'
-import {TeliGram} from 'src/modules/telegram/telegram.controller';
-import {BotService} from 'src/modules/telegram/telegram.service'
+import {LogsController} from './modules/logs/logs.controller';
+import {LogsService} from './modules/logs/logs.service';
+import {TeliGram} from './modules/telegram/telegram.controller';
+import {BotService} from './modules/telegram/telegram.service';
 
 @Module({
   imports: [
